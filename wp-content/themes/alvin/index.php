c91176cbeb447a62b2d4c47dec903acc81de8200
alvinyw <alvin.yiwen@gmail.com>
2019-07-05 15:01:01 +0800

    change ‘最新’ to ‘最热’

--- wp-content/themes/alvin/index.php
+++ wp-content/themes/alvin/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
         <?php div_cotent(); ?> 
         		<div id="index" class="bs uk-text-break">
-        			<h4><?php echo of_get('index_list_text')? of_get('index_list_text') :'最新文章';?></h4>
+        			<h4><?php echo of_get('index_list_text')? of_get('index_list_text') :'最热文章';?></h4>
         			<div id="list">
 						<?php index_loop();?>
         			</div>
